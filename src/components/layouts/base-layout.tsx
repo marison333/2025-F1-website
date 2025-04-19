a4a596b551748f24a79ec8391f8c8187e9611886
marison333 <marison.sol@outlook.com>
2025-04-19 19:28:02 +0200

    base layout

---
+++ src/components/layouts/base-layout.tsx
@@ -0,0 +1,11 @@
+export default function BaseLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      {children}
+    </div>
+  );
+}
