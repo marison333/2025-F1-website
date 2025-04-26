63217bda9e1a41262bf6210a3aa1dca9dc01764a
marison333 <marison.sol@outlook.com>
2025-04-26 12:12:36 +0200

    style borders nav

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -10,47 +10,49 @@ import { Button } from '../ui/button';
 
 export default function Navigation() {
     return (
-        <NavigationMenu className='md:max-w-7xl flex justify-between p-3 mx-auto'>
-            <NavigationMenuList>
-                <NavigationMenuItem className='hover:cursor-pointer'>
-                    <Link href='/' passHref>
-                        <p className='font-bold text-4xl'>F1 Tracker</p>
-                    </Link>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <Link className={navigationMenuTriggerStyle()} href='/'>
-                        Home
-                    </Link>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <Link className={navigationMenuTriggerStyle()} href='#'>
-                        Drivers
-                    </Link>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <Link className={navigationMenuTriggerStyle()} href='#'>
-                        Teams
-                    </Link>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <Link className={navigationMenuTriggerStyle()} href='#'>
-                        Standings
-                    </Link>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <Link className={navigationMenuTriggerStyle()} href='#'>
-                        Circuits
-                    </Link>
-                </NavigationMenuItem>
-            </NavigationMenuList>
+        <NavigationMenu className='md:max-w-7xl flex p-2 my-2 mx-auto border-2 rounded-lg'>
+            <div className='w-full p-1 flex justify-between border-2 rounded-md'>
+                <NavigationMenuList>
+                    <NavigationMenuItem className='hover:cursor-pointer'>
+                        <Link href='/' passHref>
+                            <p className='font-bold text-4xl'>F1 Tracker</p>
+                        </Link>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <Link className={navigationMenuTriggerStyle()} href='/'>
+                            Home
+                        </Link>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <Link className={navigationMenuTriggerStyle()} href='#'>
+                            Drivers
+                        </Link>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <Link className={navigationMenuTriggerStyle()} href='#'>
+                            Teams
+                        </Link>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <Link className={navigationMenuTriggerStyle()} href='#'>
+                            Standings
+                        </Link>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <Link className={navigationMenuTriggerStyle()} href='#'>
+                            Circuits
+                        </Link>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
 
-            <NavigationMenuList>
-                <NavigationMenuItem>
-                    <Button variant='outline' className='w-full h-full'>
-                        In beta
-                    </Button>
-                </NavigationMenuItem>
-            </NavigationMenuList>
+                <NavigationMenuList>
+                    <NavigationMenuItem>
+                        <Button variant='outline' className='w-full h-full'>
+                            In beta
+                        </Button>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </div>
         </NavigationMenu>
     );
 }
