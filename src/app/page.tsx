306d9445e4c62d7d7eb3a40566be14bb7ca85b13
marison333 <marison.sol@outlook.com>
2025-04-26 11:50:57 +0200

    init landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,46 +3,56 @@ import Link from 'next/link';
 
 export default function Home() {
     return (
-        <div className='w-full h-[30rem] flex gap-2'>
-            <Card className='max-w-3xl flex-1'>
-                <CardContent>
-                    <h1 className='text-6xl font-bold'>Welcome to Formule 1 Tracker</h1>
-                    <p className='mt-2 text-lg'>Track your favorite Formula 1 stats, drivers, teams and Seasons</p>
-                </CardContent>
-            </Card>
-
-            <div className='grid grid-cols-2 grid-rows-7 gap-2 max-w-3xl flex-1'>
-                <Card className='col-span-2 py-0'>
+        <>
+            <div className='w-full h-[30rem] flex gap-2'>
+                <Card className='max-w-3xl flex-1'>
                     <CardContent>
-                        <h3 className='font-bold text-4xl text-center'>Track any</h3>
+                        <h1 className='text-6xl font-bold'>Welcome to Formule 1 Tracker</h1>
+                        <p className='mt-2 text-lg'>
+                            Track your favorite Formula 1 stats, drivers, teams and Seasons
+                        </p>
                     </CardContent>
                 </Card>
-                <Card className='row-span-4'>
-                    <Link className='block w-full h-full' href='/drivers'>
-                        <span className='text-center text-2xl font-bold'>Driver</span>
-                    </Link>
-                </Card>
-                <Card className='row-span-2'>
-                    <Link className='block w-full h-full' href='/teams'>
-                        <span className='text-center text-2xl font-bold'>Team</span>
-                    </Link>
-                </Card>
-                <Card className='row-span-2'>
-                    <Link className='block w-full h-full' href='/seasons'>
-                        <span className='text-center text-2xl font-bold'>Season</span>
-                    </Link>
-                </Card>
-                <Card className='row-span-2'>
-                    <Link className='block w-full h-full' href='/circuits'>
-                        <span className='text-center text-2xl font-bold'>Circuit</span>
-                    </Link>
-                </Card>
-                <Card className='row-span-2'>
-                    <Link className='block w-full h-full' href='/stats'>
-                        <span className='text-center text-2xl font-bold'>Other Statistics</span>
-                    </Link>
-                </Card>
+                <div className='grid grid-cols-2 grid-rows-7 gap-2 max-w-3xl flex-1'>
+                    <Card className='col-span-2 py-0'>
+                        <CardContent>
+                            <h3 className='font-bold text-4xl text-center'>Track any</h3>
+                        </CardContent>
+                    </Card>
+                    <Card className='row-span-4'>
+                        <Link className='block w-full h-full' href='/drivers'>
+                            <span>Driver</span>
+                        </Link>
+                    </Card>
+                    <Card className='row-span-2'>
+                        <Link className='block w-full h-full' href='/teams'>
+                            <span>Team</span>
+                        </Link>
+                    </Card>
+                    <Card className='row-span-2'>
+                        <Link className='block w-full h-full' href='/seasons'>
+                            <span>Season</span>
+                        </Link>
+                    </Card>
+                    <Card className='row-span-2'>
+                        <Link className='block w-full h-full' href='/circuits'>
+                            <span>Circuit</span>
+                        </Link>
+                    </Card>
+                    <Card className='row-span-2'>
+                        <Link className='block w-full h-full' href='/stats'>
+                            <span>Other Statistics</span>
+                        </Link>
+                    </Card>
+                </div>
             </div>
-        </div>
+            <div>
+                <h2 className='font-bold text-5xl'>Find all details right here</h2>
+                <div>
+                    <h3>Any driver, Any time</h3>
+                    <p>Find all the details about your favorite driver, including their stats, teams, and seasons.</p>
+                </div>
+            </div>
+        </>
     );
 }
