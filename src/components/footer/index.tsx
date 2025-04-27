5822d64eecacf14c79100e69565699022721fd6c
marison333 <marison.sol@outlook.com>
2025-04-27 14:10:40 +0200

    change(components/home) created footer comp (WIP)

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,66 @@
+import Link from 'next/link';
+
+import { Card } from '../ui/card';
+import { Separator } from '../ui/separator';
+
+export default function Footer() {
+    return (
+        <footer>
+            <Card>
+                <div className='p-4'>
+                    <div className='gap-4 mb-4'>
+                        <div className='w-[50rem] h-5xl'>
+                            <h2 className='text-2xl font-bold'>F1 Tracker</h2>
+                            <p className='text-sm text-muted-foreground'>
+                                This is a sample footer. You can add your own content here.
+                            </p>
+                        </div>
+                        <div className='w-[50rem] h-[15rem]'>
+                            <h2 className='text-2xl'>Navigate</h2>
+                            <ol className='list-none'>
+                                <li>
+                                    <Link href='#'>Home</Link>
+                                </li>
+                                <li>
+                                    <Link href='#'>Drivers</Link>
+                                </li>
+                                <li>
+                                    <Link href='#'>Teams</Link>
+                                </li>
+                                <li>
+                                    <Link href='#'>Circuits</Link>
+                                </li>
+                            </ol>
+                        </div>
+                    </div>
+                    <Separator />
+                    <div className='flex justify-between items-center mt-4'>
+                        <h3>
+                            Build By {' '}
+                            <a href='#' target='_blank'>
+                                @Marison
+                            </a>
+                        </h3>
+                        <ol className='flex gap-4 list-none'>
+                            <li>
+                                <a href='' target='_blank'>
+                                    Github
+                                </a>
+                            </li>
+                            <li>
+                                <a href='' target='_blank'>
+                                    X
+                                </a>
+                            </li>
+                            <li>
+                                <a href='' target='_blank'>
+                                    Instagram
+                                </a>
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+            </Card>
+        </footer>
+    );
+}
---
+++ src/components/landing-page/index.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import Link from 'next/link';
+
+import { Card, CardContent } from '@/components/ui/card';
+
+export default function LandingPage() {
+    return (
+        <>
+            <div className='w-full h-[30rem] flex gap-2'>
+                <Card className='max-w-3xl flex-1'>
+                    <CardContent>
+                        <h1 className='text-6xl font-bold'>Welcome to Formule 1 Tracker</h1>
+                        <p className='mt-2 text-lg'>
+                            Track your favorite Formula 1 stats, drivers, teams and Seasons
+                        </p>
+                    </CardContent>
+                </Card>
+                <div className='grid grid-cols-2 grid-rows-7 gap-2 max-w-3xl flex-1'>
+                    <Card className='col-span-2 py-0'>
+                        <CardContent>
+                            <h3 className='font-bold text-4xl text-center'>Track any</h3>
+                        </CardContent>
+                    </Card>
+                    <Card className='row-span-4'>
+                        <Link className='block w-full h-full' href='/drivers'>
+                            <span>Driver</span>
+                        </Link>
+                    </Card>
+                    <Card className='row-span-2'>
+                        <Link className='block w-full h-full' href='/teams'>
+                            <span>Team</span>
+                        </Link>
+                    </Card>
+                    <Card className='row-span-2'>
+                        <Link className='block w-full h-full' href='/seasons'>
+                            <span>Season</span>
+                        </Link>
+                    </Card>
+                    <Card className='row-span-2'>
+                        <Link className='block w-full h-full' href='/circuits'>
+                            <span>Circuit</span>
+                        </Link>
+                    </Card>
+                    <Card className='row-span-2'>
+                        <Link className='block w-full h-full' href='/stats'>
+                            <span>Other Statistics</span>
+                        </Link>
+                    </Card>
+                </div>
+            </div>
+            <div>
+                <h2 className='font-bold text-5xl'>Find all details right here</h2>
+                <div>
+                    <h3>Any driver, Any time</h3>
+                    <p>
+                        Find all the details about your favorite driver, including their stats,
+                        teams, and seasons.
+                    </p>
+                </div>
+            </div>
+        </>
+    );
+}
