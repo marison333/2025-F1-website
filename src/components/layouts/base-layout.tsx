93e0ca6c12b0d930606b1bb3a7b79042d7984120
marison333 <marison.sol@outlook.com>
2025-05-17 14:20:47 +0200

    chore(styling) layout centering for medium screens

--- src/components/layouts/base-layout.tsx
+++ src/components/layouts/base-layout.tsx
@@ -3,5 +3,5 @@ export default function BaseLayout({
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-    return <div className='max-w-[90rem] lg:mx-auto'>{children}</div>;
+    return <div className='max-w-[90rem] md:mx-auto'>{children}</div>;
 }
