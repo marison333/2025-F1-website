3e9839ff9be0e596b4a8b1576c157e1973ed0f96
marison333 <marison.sol@outlook.com>
2025-06-23 22:07:13 +0200

    feat(mock): new nationality mock data

---
+++ src/mock/nationality.ts
@@ -0,0 +1,116 @@
+import { Nationality } from '../types/index';
+
+const nationality: Nationality[] = [
+    {
+        code: 'FR',
+        name: 'French',
+        iconUrl: `/icons/flags/fr.svg`
+    },
+    {
+        code: 'GB',
+        name: 'British',
+        iconUrl: `/icons/flags/gb.svg`
+    },
+    {
+        code: 'IT',
+        name: 'Italian',
+        iconUrl: `/icons/flags/it.svg`
+    },
+    {
+        code: 'DE',
+        name: 'German',
+        iconUrl: `/icons/flags/de.svg`
+    },
+    {
+        code: 'US',
+        name: 'American',
+        iconUrl: `/icons/flags/us.svg`
+    },
+    {
+        code: 'ES',
+        name: 'Spanish',
+        iconUrl: `/icons/flags/es.svg`
+    },
+    {
+        code: 'NL',
+        name: 'Dutch',
+        iconUrl: `/icons/flags/nl.svg`
+    },
+    {
+        code: 'JP',
+        name: 'Japanese',
+        iconUrl: `/icons/flags/jp.svg`
+    },
+    {
+        code: 'AU',
+        name: 'Australian',
+        iconUrl: `/icons/flags/au.svg`
+    },
+    {
+        code: 'BR',
+        name: 'Brazilian',
+        iconUrl: `/icons/flags/br.svg`
+    },
+    {
+        code: 'CA',
+        name: 'Canadian',
+        iconUrl: `/icons/flags/ca.svg`
+    },
+    {
+        code: 'CH',
+        name: 'Swiss',
+        iconUrl: `/icons/flags/ch.svg`
+    },
+    {
+        code: 'AT',
+        name: 'Austrian',
+        iconUrl: `/icons/flags/at.svg`
+    },
+    {
+        code: 'PT',
+        name: 'Portuguese',
+        iconUrl: `/icons/flags/pt.svg`
+    },
+    {
+        code: 'CN',
+        name: 'Chinese',
+        iconUrl: `/icons/flags/cn.svg`
+    },
+    {
+        code: 'IN',
+        name: 'Indian',
+        iconUrl: `/icons/flags/in.svg`
+    },
+    {
+        code: 'MX',
+        name: 'Mexican',
+        iconUrl: `/icons/flags/mx.svg`
+    },
+    {
+        code: 'MC',
+        name: 'Monacan',
+        iconUrl: `/icons/flags/mc.svg`
+    },
+    {
+        code: 'TH',
+        name: 'Thai',
+        iconUrl: `/icons/flags/th.svg`
+    },
+    {
+        code: 'GM',
+        name: 'German',
+        iconUrl: `/icons/flags/gm.svg`
+    },
+    {
+        code: 'NZ',
+        name: 'New Zealander',
+        iconUrl: `/icons/flags/nz.svg`
+    },
+    {
+        code: 'AR',
+        name: 'Argentinian',
+        iconUrl: `/icons/flags/ar.svg`
+    }
+];
+
+export default nationality;
