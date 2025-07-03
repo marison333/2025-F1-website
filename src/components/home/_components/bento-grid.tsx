9f08f074e2e06c285583749fb7e07949da0f70bd
marison333 <marison.sol@outlook.com>
2025-07-03 12:29:25 +0200

    feat(home): updated bento grid component

--- src/components/home/_components/bento-grid.tsx
+++ src/components/home/_components/bento-grid.tsx
@@ -5,111 +5,138 @@ import { Card } from '@/components/ui/card';
 
 export default function BentoGrid() {
     return (
-        <section>
-            <div className='max-w-3xl p-4 mb-5'>
-                <h2 className='text-4xl font-bold mb-4'>
-                    Explore every detail of your of Formule 1
-                </h2>
-                <p className='text-lg text-muted-foreground mb-6'>
-                    Discover the latest drivers, teams, and standings in the world of Formule 1.
-                </p>
-            </div>
-
-            <div className='h-[50rem] flex justify-between gap-2 mb-5'>
-                <div className='max-w-3xl p-4 flex-1'>
-                    <h3 className='text-8xl font-bold'>Welcome to Formule 1 Tracker</h3>
-                    <p className='mt-2 text-lg text-muted-foreground'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae impedit
-                        veritatis, blanditiis praesentium quo quas est velit earum aspernatur ipsa
-                        quis maxime neque dicta consequuntur architecto quia facilis, iusto ea.
-                    </p>
-                </div>
-
-                <div className='grid grid-cols-2 grid-rows-7 max-w-3xl flex-1 gap-1 [&>*]:relative [&>*]:overflow-hidden h-[50rem] p-1'>
-                    <Card aria-label='View Drivers' className='row-span-4'>
-                        <Link
-                            aria-label='View Drivers link'
-                            className='block w-full h-full'
-                            href='/drivers'>
-                            <Image
-                                alt='picture of formule 1 race driver Lewis Hamilton'
-                                className='w-full h-full object-cover transition-transform duration-300 hover:scale-105'
-                                fill
-                                priority
-                                sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-                                src='/images/home/hero/driver.jpg'
-                            />
+        <div className='py-10' data-slot='bento-grid'>
+            <h2 className='uppercase'>Find what you like</h2>
+            <div
+                className='relative w-full grid grid-cols-2 grid-rows-2 gap-4 max-h-[40rem] [&>*]:h-[20rem]'
+                data-slot='bento-grid'>
+                <Card className='flex col-span-1 overflow-hidden relative'>
+                    <span className='flex-1'>
+                        <Image
+                            src='/images/home/bento-grid/battle.png'
+                            alt='Image of 2 Formula 1 Cars Battling'
+                            width={500}
+                            height={500}
+                            className='object-cover w-full h-full'
+                        />
+                    </span>
+                    <div className='bg-gradient-to-b from-transparent to-black absolute z-20 bottom-0 left-0 right-0 p-4 w-full'>
+                        <span className='text-white text-2xl font-semibold'>
+                            Look at all statistics right here
+                        </span>
+                    </div>
+                </Card>
+                <div className='grid grid-cols-2 gap-4 [&>*]:overflow-hidden'>
+                    <Card className='group'>
+                        <Link href='#'>
+                            <div>
+                                <div>
+                                    <Image
+                                        src='/images/home/bento-grid/standings.png'
+                                        alt='Bento Grid Image 2'
+                                        width={500}
+                                        height={500}
+                                        className='object-cover w-full h-full'
+                                    />
+                                </div>
+                                <div className='p-2'>
+                                    <h3 className='text-lg font-semibold group-hover:underline'>
+                                        Standings
+                                    </h3>
+                                    <p className='text-sm text-gray-500'>
+                                        Check the latest standings
+                                    </p>
+                                </div>
+                            </div>
                         </Link>
                     </Card>
-
-                    <Card aria-label='Track teams' className='row-span-2'>
-                        <Link
-                            aria-label='view teams link'
-                            className='block w-full h-full'
-                            href='/teams'>
-                            <Image
-                                alt='picture of formule 1 teams'
-                                className='w-full h-full object-cover transition-transform duration-300 hover:scale-105'
-                                fill
-                                priority
-                                sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-                                src='/images/home/hero/f1-teams.jpeg'
-                            />
-                        </Link>
-                    </Card>
-
-                    {/* @TODO: find apropriate pics */}
-                    <Card aria-label='View standings' className='row-span-2'>
-                        <Link
-                            aria-label='standing link'
-                            className='block w-full h-full'
-                            href='/standings'>
-                            <Image
-                                alt='standings picture'
-                                className='w-full h-full object-cover transition-transform duration-300 hover:scale-105'
-                                fill
-                                priority
-                                sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-                                src='/images/home/hero/standings.jpeg'
-                            />
-                        </Link>
-                    </Card>
-
-                    {/* @TODO: find apropriate pics */}
-                    <Card aria-label='View standings' className='row-span-2'>
-                        <Link
-                            aria-label='standing link'
-                            className='block w-full h-full'
-                            href='/standings'>
-                            <Image
-                                alt='standings picture'
-                                className='w-full h-full object-cover transition-transform duration-300 hover:scale-105'
-                                fill
-                                priority
-                                sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-                                src='/images/home/hero/circuit.jpeg'
-                            />
-                        </Link>
-                    </Card>
-
-                    {/* @TODO: find apropriate copyright pics */}
-                    <Card aria-label='Track other statistics' className='row-span-2'>
-                        <Link
-                            aria-label='standing link'
-                            className='block w-full h-full'
-                            href='/standings'>
-                            <Image
-                                alt='standings picture'
-                                className='w-full h-full object-cover transition-transform duration-300 hover:scale-105'
-                                fill
-                                priority
-                                sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-                                src='/images/home/hero/history.jpg'
-                            />
+                    <Card className='group'>
+                        <Link href='#'>
+                            <div>
+                                <div>
+                                    <Image
+                                        src='/images/home/bento-grid/history.jpg'
+                                        alt='Bento Grid Image 2'
+                                        width={500}
+                                        height={500}
+                                        className='object-cover w-full h-full'
+                                    />
+                                </div>
+                                <div className='p-2'>
+                                    <h3 className='text-lg font-semibold group-hover:underline'>
+                                        Explore More
+                                    </h3>
+                                    <p className='text-sm text-gray-500'>
+                                        Discover a variety of content
+                                    </p>
+                                </div>
+                            </div>
                         </Link>
                     </Card>
                 </div>
+                <div className='grid grid-cols-2 gap-4 [&>*]:group [&>*]:overflow-hidden'>
+                    <Card className='group'>
+                        {/* @TODO: Add link to driver stats */}
+                        <Link href='#'>
+                            <div>
+                                <Image
+                                    src='/images/home/bento-grid/driver.png'
+                                    alt='Bento Grid Image 2'
+                                    width={500}
+                                    height={500}
+                                    className='object-cover w-full h-full'
+                                />
+                            </div>
+                            <div className='p-2'>
+                                <h3 className='text-lg font-semibold group-hover:underline'>
+                                    Find your driver
+                                </h3>
+                                <p className='text-sm text-gray-500'>
+                                    Search for your favorite driver and see their stats
+                                </p>
+                            </div>
+                        </Link>
+                    </Card>
+                    <Card className='group'>
+                        {/* @TODO: Add link to team stats */}
+                        <Link href='#'>
+                            <div>
+                                <Image
+                                    src='/images/home/bento-grid/team.jpeg'
+                                    alt='Bento Grid Image 2'
+                                    width={500}
+                                    height={500}
+                                    className='object-cover w-full h-full'
+                                />
+                            </div>
+                            <div className='p-2'>
+                                <h3 className='text-lg font-semibold group-hover:underline'>
+                                    Find your team
+                                </h3>
+                                <p className='text-sm text-gray-500'>
+                                    Search for your favorite team and see their stats
+                                </p>
+                            </div>
+                        </Link>
+                    </Card>
+                </div>
+                <Card className='flex col-span-1 overflow-hidden relative'>
+                    <span className='flex-1'>
+                        <Image
+                            src='/images/home/bento-grid/circuit.jpeg'
+                            alt='Image of a Formula 1 Circuit'
+                            width={500}
+                            height={500}
+                            className='object-cover w-full h-full'
+                        />
+                    </span>
+                    <div className='bg-gradient-to-b from-transparent to-black absolute z-20 bottom-0 left-0 right-0 p-4 w-full'>
+                        <span className='text-white text-2xl font-semibold'>
+                            Explore the circuits
+                        </span>
+                    </div>
+                </Card>
             </div>
-        </section>
+        </div>
     );
 }
