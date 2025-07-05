163f41d740e62deba1455857dad252d897fb2966
marison333 <marison.sol@outlook.com>
2025-07-05 12:56:05 +0200

    feat(style): added custom f1 team colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,20 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
     content: ['./src/**/*.{ts,tsx}'],
     theme: {
-        extend: {}
+        extend: {
+            colors: {
+                alpine: '#009ee4',
+                astonMartin: '#22976e',
+                ferrari: '#ce3035',
+                haas: '#9b9da0',
+                kickSauber: '#00bc0d',
+                mclaren: '#f17602',
+                mercedes: '#00d5b5',
+                racingBulls: '#457ed4',
+                redBull: '#457ed4',
+                williams: '#1963d9'
+            }
+        }
     },
     plugins: []
 };
