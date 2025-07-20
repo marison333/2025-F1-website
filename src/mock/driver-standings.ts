6da862e285947c9ec180c42baaae7fd49b73964a
marison333 <marison.sol@outlook.com>
2025-07-20 10:34:13 +0200

    chore(mock): simplified standings mock data

--- src/mock/driver-standings.ts
+++ src/mock/driver-standings.ts
@@ -3,124 +3,84 @@ import drivers from './drivers';
 
 const driverStandings: DriverStanding[] = [
     {
-        1: {
-            driver: drivers[12],
-            points: 234
-        }
+        driver: drivers[12],
+        points: 234
     },
     {
-        2: {
-            driver: drivers[4],
-            points: 226
-        }
+        driver: drivers[4],
+        points: 226
     },
     {
-        3: {
-            driver: drivers[1],
-            points: 165
-        }
+        driver: drivers[1],
+        points: 165
     },
     {
-        4: {
-            driver: drivers[6],
-            points: 147
-        }
+        driver: drivers[6],
+        points: 147
     },
     {
-        5: {
-            driver: drivers[2],
-            points: 120
-        }
+        driver: drivers[2],
+        points: 120
     },
     {
-        6: {
-            driver: drivers[0],
-            points: 103
-        }
+        driver: drivers[0],
+        points: 103
     },
     {
-        7: {
-            driver: drivers[14],
-            points: 63
-        }
+        driver: drivers[14],
+        points: 63
     },
     {
-        8: {
-            driver: drivers[10],
-            points: 46
-        }
+        driver: drivers[10],
+        points: 46
     },
     {
-        9: {
-            driver: drivers[11],
-            points: 72
-        }
+        driver: drivers[11],
+        points: 72
     },
     {
-        10: {
-            driver: drivers[7],
-            points: 23
-        }
+        driver: drivers[7],
+        points: 23
     },
     {
-        11: {
-            driver: drivers[17],
-            points: 21
-        }
+        driver: drivers[17],
+        points: 21
     },
     {
-        12: {
-            driver: drivers[13],
-            points: 20
-        }
+        driver: drivers[13],
+        points: 20
     },
     {
-        13: {
-            driver: drivers[8],
-            points: 19
-        }
+        driver: drivers[8],
+        points: 19
     },
     {
-        14: {
-            driver: drivers[15],
-            points: 16
-        }
+        driver: drivers[15],
+        points: 16
     },
     {
-        15: {
-            driver: drivers[3],
-            points: 13
-        }
+        driver: drivers[3],
+        points: 13
     },
     {
-        16: {
-            driver: drivers[16],
-            points: 12
-        }
+        driver: drivers[16],
+        points: 12
     },
     {
-        17: {
-            driver: drivers[9],
-            points: 10
-        }
+        driver: drivers[9],
+        points: 10
     },
     {
-        18: {
-            driver: drivers[15],
-            points: 6
-        }
+        driver: drivers[15],
+        points: 6
     },
     {
-        19: {
-            driver: drivers[18],
-            points: 4
-        }
+        driver: drivers[18],
+        points: 4
     },
     {
-        20: {
-            driver: drivers[19],
-            points: 0
-        }
+        driver: drivers[19],
+        points: 0
     }
 ];
 
--- src/mock/team-standings.ts
+++ src/mock/team-standings.ts
@@ -3,64 +3,36 @@ import teams from './teams';
 
 const teamStandings: TeamStanding[] = [
     {
-        1: {
-            team: teams[0],
-            points: 250
-        }
+        team: teams[3],
+        points: 250
     },
     {
-        2: {
-            team: teams[1],
-            points: 200
-        }
+        team: teams[0],
+        points: 200
     },
     {
-        3: {
-            team: teams[2],
-            points: 180
-        }
+        team: teams[1],
+        points: 180
     },
     {
-        4: {
-            team: teams[3],
-            points: 150
-        }
+        team: teams[2],
+        points: 150
     },
     {
-        5: {
-            team: teams[4],
-            points: 120
-        }
+        team: teams[4],
+        points: 120
     },
     {
-        6: {
-            team: teams[5],
-            points: 100
-        }
+        team: teams[5],
+        points: 100
     },
     {
-        7: {
-            team: teams[6],
-            points: 80
-        }
+        team: teams[6],
+        points: 80
     },
     {
-        8: {
-            team: teams[7],
-            points: 60
-        }
-    },
-    {
-        9: {
-            team: teams[8],
-            points: 40
-        }
-    },
-    {
-        10: {
-            team: teams[9],
-            points: 20
-        }
+        team: teams[7],
+        points: 60
     }
 ];
 
