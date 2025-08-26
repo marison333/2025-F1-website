cb22f2687966b6c0e43e27f9f3cf5bbf34208e67
marison333 <marison.sol@outlook.com>
2025-08-26 12:19:57 +0200

    ref(home): Refactored Home component to fetch and pass driver, driver standings and team standings data to child components

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,16 +1,26 @@
-import { DriverCarousel } from './_components/carousels';
+import { getDrivers } from '@/lib/data/drivers';
+import { getDriverStandings } from '@/lib/data/driver-standings';
+import { getTeamStandings } from '@/lib/data/teams-standings';
+
+import { DriverCarousel } from './_components/driver-carousel';
 import BentoGrid from './_components/bento-grid';
 import Feedback from './_components/feedback';
 import Hero from './_components/hero';
 import Standings from './_components/standings';
 
-export default function Home() {
+export default async function Home() {
+    const data = {
+        drivers: await getDrivers(),
+        driverStandings: await getDriverStandings(),
+        teamStandings: await getTeamStandings()
+    };
+
     return (
         <>
             <Hero />
             <BentoGrid />
-            <DriverCarousel />
-            <Standings />
+            <DriverCarousel drivers={data.drivers} />
+            <Standings drivers={data.driverStandings} teams={data.teamStandings} />
             <Feedback />
         </>
     );
