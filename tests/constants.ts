26aa9c07679a44ba7c9741ffea57a0bcbb52d4f6
marison333 <marison.sol@outlook.com>
2025-09-13 21:43:47 +0200

    feat(e2e): created constants.ts for e2e tests

---
+++ tests/constants.ts
@@ -0,0 +1,22 @@
+export const driverNames: string[] = [
+    'pierre gasly',
+    'franco colapinto',
+    'fernando alonso',
+    'lance stroll',
+    'lewis hamilton',
+    'charles leclerc',
+    'esteban ocon',
+    'oliver bearman',
+    'nico hülkenberg',
+    'gabriel bortoleto',
+    'lando norris',
+    'oscar piastri',
+    'george russell',
+    'kimi antonelli',
+    'liam lawson',
+    'isack hadjar',
+    'max verstappen',
+    'yuki tsunoda',
+    'carlos sainz',
+    'alexander albon'
+];
