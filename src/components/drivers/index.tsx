0df4872c4f242335e8fc5b2fd8b2a317de25f1b8
marison333 <marison.sol@outlook.com>
2025-09-13 12:24:42 +0200

    ref(drivers): alignment adjustment & added new banner component

--- src/components/drivers/index.tsx
+++ src/components/drivers/index.tsx
@@ -1,6 +1,8 @@
 import { getDrivers } from '@/lib/data/drivers';
 import DriversPageLayout from '../layouts/drivers-page-layout';
+
 import { Overview } from './_components/overview';
+import { TeamsBanner } from './_components/banner';
 
 export default async function Drivers() {
     const driversData = await getDrivers();
@@ -8,13 +10,12 @@ export default async function Drivers() {
 
     return (
         <DriversPageLayout>
-            <div>
-                <h1 className='uppercase'>drivers 2025</h1>
-                <p>
-                    Find the current drivers of the 2025 Formula 1 season
-                </p>
+            <div className='mb-6 flex flex-col gap-1'>
+                <h1 className='uppercase font-bold'>drivers 2025</h1>
+                <p>Find the current drivers of the 2025 Formula 1 season</p>
             </div>
             <Overview drivers={driversData} />
+            <TeamsBanner />
         </DriversPageLayout>
     );
 }
