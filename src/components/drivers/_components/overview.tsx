bed8da5a348b5768cc470136f816cb08b803eeb4
marison333 <marison.sol@outlook.com>
2025-10-22 17:28:39 +0200

    ref(drivers): grid gap widend to 4px; changed `DriverCard` background

--- src/components/drivers/_components/overview.tsx
+++ src/components/drivers/_components/overview.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 import { Driver } from '@/types';
-import { teamGradientColor } from '@/utils/team-colors';
+import { getTeamBackgroundColorDark } from '@/utils/team-colors';
 
 import { Card } from '@/components/ui/card';
 import { FlagIcon } from '@/components/flag';
@@ -17,17 +17,61 @@ interface DriverCardProps {
 
 export function Overview({ drivers }: OverviewProps) {
     const DriverCard = ({ driver }: DriverCardProps) => {
+        const teamColor = (teamId: string) => {
+            return `var(--color-${teamId})`;
+        };
         const driverLink = `/drivers/${driver.givenName.toLowerCase()}-${driver.familyName.toLowerCase()}`;
-        
+
         return (
             <Link data-slot='driver-card' href={driverLink}>
                 <Card
-                    className={`group h-[14rem] w-full text-white bg-gradient-to-br ${teamGradientColor(driver.team.id)} to-stone-800`}>
-                    <div className='w-full p-1.5 flex justify-between overflow-hidden'>
+                    className={`relative z-0 group h-[14rem] w-full min-w-[300px] text-white ${getTeamBackgroundColorDark(driver.team.id)} overflow-hidden`}>
+                    <div className='w-[1122px] h-[306px] absolute top-2/4 -translate-y-2/4 left-[31px] md:left-2/4 md:-translate-x-2/4 md:opacity-95'>
+                        <div className='absolute h-[306px] left-0 md:left-[43%] md:right-0 top-2/4 -translate-y-2/4'>
+                            <span
+                                className='block aspect-[2.0284528749]'
+                                style={{
+                                    backgroundColor: teamColor(driver.team.id),
+                                    height: '100%',
+                                    maskImage: 'url(/images/drivers/background.webp)',
+                                    maskPosition: 'center',
+                                    maskRepeat: 'no-repeat',
+                                    maskSize: 'contain',
+                                    opacity: 1,
+                                    WebkitMaskImage: 'url(/images/drivers/background.webp)',
+                                    WebkitMaskPosition: 'center',
+                                    WebkitMaskRepeat: 'no-repeat',
+                                    WebkitMaskSize: 'contain'
+                                }}
+                            />
+                        </div>
+                        <div className='hidden md:block absolute h-[306px] left-0 top-2/4 -translate-y-2/4 opacity-30'>
+                            <span
+                                className='block aspect-[2.0284528749]'
+                                style={{
+                                    backgroundColor: teamColor(driver.team.id),
+                                    height: '100%',
+                                    maskImage: 'url(/images/drivers/background.webp)',
+                                    maskPosition: 'center',
+                                    maskRepeat: 'no-repeat',
+                                    maskSize: 'contain',
+                                    opacity: 1,
+                                    WebkitMaskImage: 'url(/images/drivers/background.webp)',
+                                    WebkitMaskPosition: 'center',
+                                    WebkitMaskRepeat: 'no-repeat',
+                                    WebkitMaskSize: 'contain'
+                                }}
+                            />
+                        </div>
+                    </div>
+
+                    <div className='z-10 w-full p-1.5 flex justify-between overflow-hidden'>
                         <div className='flex flex-col justify-between w-max p-2'>
                             <div className='h-full flex flex-col justify-between'>
                                 <div className='flex flex-col'>
-                                    <span className='flex flex-col gap-1 capitalize leading-5 text-2xl  group-hover:underline' data-testid='driver-name'>
+                                    <span
+                                        className='flex flex-col gap-1 capitalize leading-5 text-2xl  group-hover:underline'
+                                        data-testid='driver-name'>
                                         <span>{driver.givenName}</span>
                                         <span className='font-bold'>{driver.familyName}</span>
                                     </span>
@@ -44,7 +88,7 @@ export function Overview({ drivers }: OverviewProps) {
                                 />
                             </div>
                         </div>
-                        <div className='w-max'>
+                        <div>
                             <Image
                                 alt={`Picture of Formula One ${driver.givenName} ${driver.familyName}`}
                                 className='object-center w-max'
@@ -53,7 +97,7 @@ export function Overview({ drivers }: OverviewProps) {
                                 width={150}
                             />
                         </div>
-                        <div></div>
+                        <div />
                     </div>
                 </Card>
             </Link>
@@ -62,7 +106,7 @@ export function Overview({ drivers }: OverviewProps) {
 
     return (
         <ul
-            className='w-full flex flex-col gap-3 list-none [@media(min-width:679px)]:grid [@media(min-width:679px)]:grid-cols-2'
+            className='w-full flex flex-col gap-4 list-none [@media(min-width:679px)]:grid [@media(min-width:679px)]:grid-cols-2'
             data-slot='driver-list'>
             {drivers.map((driver) => (
                 <li key={driver.code}>
