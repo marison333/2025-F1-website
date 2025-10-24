631cf03c4ad9f61411d50d9bb96bc99637116e49
marison333 <marison.sol@outlook.com>
2025-10-24 11:21:16 +0200

    feat(e2e): created `viewports.ts` for e2e testing

---
+++ tests/constants/viewports.ts
@@ -0,0 +1,7 @@
+export const viewports = {
+    mobile: { width: 375, height: 812 },
+    tablet: { width: 768, height: 1024 },
+    laptop: { width: 1024, height: 768 },
+    desktop: { width: 1280, height: 800 },
+    widescreen: { width: 1440, height: 900 }
+};
