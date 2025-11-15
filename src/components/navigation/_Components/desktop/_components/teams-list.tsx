2cb49d8967f11e1b17714300b9b11e0507812a4a
marison333 <marison.sol@outlook.com>
2025-11-15 12:06:59 +0100

    feat(nav): added masked-bg accents to all teams nav dropdown links

--- src/components/navigation/_Components/desktop/_components/teams-list.tsx
+++ src/components/navigation/_Components/desktop/_components/teams-list.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { Team } from '@/types';
-import { teamBackgroundColor, teamGradientColor } from '@/utils/team-colors';
+import { getTeamBackgroundColorDark } from '@/utils/team-colors';
 
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
@@ -17,12 +17,73 @@ type TeamCardProps = {
 };
 
 const TeamCard = ({ team }: TeamCardProps) => {
+    const logoBackgroundColor: string = getTeamBackgroundColorDark(team.id);
+    const teamVarients: Record<string, string> = {
+        alpine: 'group-hover:bg-alpine',
+        'aston-martin': 'group-hover:bg-aston-martin',
+        ferrari: 'group-hover:bg-ferrari',
+        haas: 'group-hover:bg-haas',
+        'kick-sauber': 'group-hover:bg-kick-sauber',
+        mclaren: 'group-hover:bg-mclaren',
+        mercedes: 'group-hover:bg-mercedes',
+        'racing-bulls': 'group-hover:bg-racing-bulls',
+        'red-bull': 'group-hover:bg-red-bull',
+        williams: 'group-hover:bg-williams'
+    };
+    const teamColor = (teamId: string) => {
+        return `var(--color-${teamId})`;
+    };
+
     return (
         <>
-            <Link className='overflow-hidden' data-slot='team-navigation-card' href={`/teams/${team.id}`}>
-                <Card className={`w-full h-[9rem] border-stone-800 dark bg-gradient-to-t ${teamGradientColor(team.id)} from-50% to-stone-800`}>
-                    <CardHeader className='flex items-center gap-1 w-full p-2'>
-                        <span className={`p-0.5 rounded-full shadow ${teamBackgroundColor(team.id)}`}>
+            <Link
+                className='group relative overflow-hidden'
+                data-slot='team-navigation-card'
+                href={`/teams/${team.id}`}>
+                <Card className={`w-full h-36 border-stone-800 dark`}>
+                    <span
+                        className={`absolute z-10 top-0 left-0 inset-10 size-full ${teamVarients[team.id]} opacity-[0.03] group-hover:opacity-[0.12]`}
+                    />
+                    <span
+                        className={`z-10 absolute block h-[66px] -top-[27px] -right-[165px] opacity-[0.1] group-hover:opacity-30`}>
+                        <span
+                            className='block aspect-[6.0981595092]'
+                            style={{
+                                backgroundColor: teamColor(team.id),
+                                height: '100%',
+                                maskImage: 'url(/images/navigation/bg-team.webp)',
+                                maskPosition: 'center',
+                                maskRepeat: 'no-repeat',
+                                maskSize: 'contain',
+                                opacity: 1,
+                                WebkitMaskImage: 'url(/images/navigation/bg-team.webp)',
+                                WebkitMaskPosition: 'center',
+                                WebkitMaskRepeat: 'no-repeat',
+                                WebkitMaskSize: 'contain'
+                            }}
+                        />
+                    </span>
+                    <span className='z-10 absolute block h-16 -bottom-[9px] -left-6 opacity-[0.1] group-hover:opacity-40'>
+                        <span
+                            className='block aspect-[6.0981595092]'
+                            style={{
+                                backgroundColor: teamColor(team.id),
+                                height: '100%',
+                                maskImage: 'url(/images/navigation/bg-team.webp)',
+                                maskPosition: 'center',
+                                maskRepeat: 'no-repeat',
+                                maskSize: 'contain',
+                                opacity: 1,
+                                WebkitMaskImage: 'url(/images/navigation/bg-team.webp)',
+                                WebkitMaskPosition: 'center',
+                                WebkitMaskRepeat: 'no-repeat',
+                                WebkitMaskSize: 'contain'
+                            }}
+                        />
+                    </span>
+
+                    <CardHeader className='z-20 flex items-center gap-1 w-full p-2'>
+                        <span className={`p-0.5 rounded-full shadow ${logoBackgroundColor}`}>
                             <Image
                                 alt={`${team.name} logo`}
                                 className='object-contain'
@@ -36,7 +97,7 @@ const TeamCard = ({ team }: TeamCardProps) => {
                             {team.id.replace(/-/g, ' ')}
                         </span>
                     </CardHeader>
-                    <CardContent>
+                    <CardContent className='z-20'>
                         <Image
                             alt={`${team.name}`}
                             className='object-contain size-full'
