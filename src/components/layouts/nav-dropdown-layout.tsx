b11797c7951fb6b526b7eeee50c4af537916b38a
marison333 <marison.sol@outlook.com>
2025-11-15 12:08:48 +0100

    chore(nav): added `z-index` to `NavDropdownLayout`

--- src/components/layouts/nav-dropdown-layout.tsx
+++ src/components/layouts/nav-dropdown-layout.tsx
@@ -1,3 +1,3 @@
 export default function NavDropdownLayout({ children }: { children: React.ReactNode }) {
-    return <div className='max-w-[90rem] mx-auto'>{children}</div>;
+    return <div className='z-90 max-w-[90rem] mx-auto'>{children}</div>;
 }
