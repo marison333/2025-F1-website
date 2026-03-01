55fea21fe3c840dfccf3c00b860a3cbc30a5a710
marison333 <marison.sol@outlook.com>
2026-03-01 21:25:43 +0100

    feat: added animation to the both drivers & teams overview page

--- src/components/teams/_components/banner.tsx
+++ src/components/teams/_components/banner.tsx
@@ -2,20 +2,29 @@ import Link from 'next/link';
 
 import { Button } from '@/components/ui/button';
 import { Card } from '@/components/ui/card';
+import { InView } from '@/components/ui/in-view';
 
 export const DriverBanner = () => {
     return (
-        <Card className='h-[20rem] p-5 my-10 bg-amber-600 text-white' data-slot='driver-banner'>
-            <div className='w-full mt-auto'>
-                <h2 className='uppercase text-4xl font-extrabold'>2025 f1 drivers</h2>
-                <p>
-                    Check out this season&apos;s official F1 line-up by following your favourite F1
-                    drivers on and off the track.
-                </p>
-                <Button asChild className='bg-white text-black' size='lg'>
-                    <Link href='/drivers'>View drivers</Link>
-                </Button>
-            </div>
-        </Card>
+        <InView
+            variants={{
+                hidden: { opacity: 0, y: 100, filter: 'blur(4px)' },
+                visible: { opacity: 1, y: 0, filter: 'blur(0px)' }
+            }}
+            viewOptions={{ margin: '0px 0px -200px 0px' }}
+            transition={{ duration: 0.3, ease: 'easeInOut' }}>
+            <Card className='h-[20rem] p-5 my-10 bg-amber-600 text-white' data-slot='driver-banner'>
+                <div className='w-full mt-auto'>
+                    <h2 className='uppercase text-4xl font-extrabold'>2025 f1 drivers</h2>
+                    <p>
+                        Check out this season&apos;s official F1 line-up by following your favourite
+                        F1 drivers on and off the track.
+                    </p>
+                    <Button asChild className='bg-white text-black' size='lg'>
+                        <Link href='/drivers'>View drivers</Link>
+                    </Button>
+                </div>
+            </Card>
+        </InView>
     );
 };
--- src/components/teams/_components/team-card.tsx
+++ src/components/teams/_components/team-card.tsx
@@ -5,6 +5,7 @@ import { Team } from '@/types';
 import { teamGradientColor, darkTeamGradientColor } from '@/utils/team-colors';
 
 import { Card } from '@/components/ui/card';
+import { InView } from '@/components/ui/in-view';
 
 interface TeamCardProps {
     team: Team;
@@ -18,57 +19,65 @@ export const TeamCard = ({ team }: TeamCardProps) => {
     };
 
     return (
-        <Link data-slot='team-card' href={`/teams/${team.id}`} className='relative group'>
-            <Card
-                className={`relative h-[15rem] w-full p-4 gap-0 justify-between text-white overflow-hidden bg-gradient-to-l ${teamGradientColor(team.id)} ${darkTeamGradientColor(team.id)}`}>
-                <span className='absolute h-[8rem] lg:h-[9rem] -bottom-[2px] -left[800px] lg:left-inherit lg:right-0 opacity-70'>
-                    <span
-                        className='block aspect-[6.0981595092]'
-                        style={{
-                            backgroundColor: teamColor(team.id),
-                            height: '100%',
-                            maskImage: 'url(/images/teams/background.webp)',
-                            maskPosition: 'center',
-                            maskRepeat: 'no-repeat',
-                            maskSize: 'contain',
-                            opacity: 1
-                        }}
-                    />
-                </span>
-                <span className='relative h-fit flex justify-between'>
-                    <span>
-                        <h3 className='capitalize group-hover:underline'>{teamDisplayName}</h3>
+        <InView
+            variants={{
+                hidden: { opacity: 0, y: 100, filter: 'blur(4px)' },
+                visible: { opacity: 1, y: 0, filter: 'blur(0px)' }
+            }}
+            viewOptions={{ margin: '0px 0px -200px 0px' }}
+            transition={{ duration: 0.3, ease: 'easeInOut' }}>
+            <Link data-slot='team-card' href={`/teams/${team.id}`} className='relative group'>
+                <Card
+                    className={`relative h-[15rem] w-full p-4 gap-0 justify-between text-white overflow-hidden bg-gradient-to-l ${teamGradientColor(team.id)} ${darkTeamGradientColor(team.id)}`}>
+                    <span className='absolute h-[8rem] lg:h-[9rem] -bottom-[2px] -left[800px] lg:left-inherit lg:right-0 opacity-70'>
                         <span
-                            className='w-full md:flex md:flex-row md:gap-2'
-                            data-testid='team-drivers'>
-                            {drivers.map((driver, index) => (
-                                <span className='flex flex-wrap gap-0.5' key={index}>
-                                    <span>{driver.givenName}</span>
-                                    <span className='font-black'>{driver.familyName}</span>
-                                </span>
-                            ))}
-                        </span>
-                    </span>
-                    <span className='size-11 p-1.5 flex items-center justify-center rounded-full bg-black/50'>
-                        <Image
-                            alt={`${team.id} icon`}
-                            className='object-contain flex items-center'
-                            height={50}
-                            src={`${team.iconUrl}`}
-                            width={50}
+                            className='block aspect-[6.0981595092]'
+                            style={{
+                                backgroundColor: teamColor(team.id),
+                                height: '100%',
+                                maskImage: 'url(/images/teams/background.webp)',
+                                maskPosition: 'center',
+                                maskRepeat: 'no-repeat',
+                                maskSize: 'contain',
+                                opacity: 1
+                            }}
                         />
                     </span>
-                </span>
-                <span className='relative h-[8rem] w-[35rem] align-bottom flex justify-end'>
-                    <Image
-                        alt={`Picture of ${team.name}'s car`}
-                        className='md:object-cover h-[7.5rem] w-full'
-                        height={450}
-                        src={`${team.carImageUrl}`}
-                        width={450}
-                    />
-                </span>
-            </Card>
-        </Link>
+                    <span className='relative h-fit flex justify-between'>
+                        <span>
+                            <h3 className='capitalize group-hover:underline'>{teamDisplayName}</h3>
+                            <span
+                                className='w-full md:flex md:flex-row md:gap-2'
+                                data-testid='team-drivers'>
+                                {drivers.map((driver, index) => (
+                                    <span className='flex flex-wrap gap-0.5' key={index}>
+                                        <span>{driver.givenName}</span>
+                                        <span className='font-black'>{driver.familyName}</span>
+                                    </span>
+                                ))}
+                            </span>
+                        </span>
+                        <span className='size-11 p-1.5 flex items-center justify-center rounded-full bg-black/50'>
+                            <Image
+                                alt={`${team.id} icon`}
+                                className='object-contain flex items-center'
+                                height={50}
+                                src={`${team.iconUrl}`}
+                                width={50}
+                            />
+                        </span>
+                    </span>
+                    <span className='relative h-[8rem] w-[35rem] align-bottom flex justify-end'>
+                        <Image
+                            alt={`Picture of ${team.name}'s car`}
+                            className='md:object-cover h-[7.5rem] w-full'
+                            height={450}
+                            src={`${team.carImageUrl}`}
+                            width={450}
+                        />
+                    </span>
+                </Card>
+            </Link>
+        </InView>
     );
 };
